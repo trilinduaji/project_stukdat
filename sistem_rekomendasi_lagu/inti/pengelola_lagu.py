8fd2db9e8c56570853677d5496131f516037c43d
dimaschndra <dimaskurniachandra70@gmail.com>
2025-12-07 09:10:29 +0700

    Update pengelola_lagu.py

--- sistem_rekomendasi_lagu/inti/pengelola_lagu.py
+++ sistem_rekomendasi_lagu/inti/pengelola_lagu.py
@@ -1,1 +1,14 @@
+def update_song_details(self, song_id, new_title, new_artist):
+    song = self.songs.get(song_id)
+    if song:
+        song.title = new_title
+        song.artist = new_artist
+        return True
+    return False
 
+def delete_song(self, song_id):
+    if song_id in self.songs:
+        song = self.songs.pop(song_id)
+        self.graph.remove_song_from_genres(song_id, song.genres)
+        return True
+    return False
